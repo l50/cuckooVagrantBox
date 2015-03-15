668f44202644b767363e9b4b828129c6eddaeb51
Aaron Gonzales <agonzales@cs.unm.edu>
2015-03-15 16:53:21 -0600

    tol for getting batches from api

---
+++ json_tools/get_json.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+## EDIT these paths
+CUCKOOUTILS=/cuckoo/utils/
+SAMPLEPATH=/cuckoo/samples/
+
+# starts the API if not started
+python $CUCKOOUTILS/api.py &
+
+cd $SAMPLEPATH
+
+FILES=./*.bytes
+
+i=1
+for f in $FILES
+do
+  filename=$(basename "$f")
+  ext="${filename##*.}"
+  filename="${filename%.*}"
+  url=http://localhost:8090/tasks/report/$i
+  httpcode=`curl -o /dev/null --silent --head --write-out '%{http_code}\n' $url`
+  if [ "$httpcode" == '200' ]; then
+    echo "Pulling results from $f"
+    curl $url > ./json/${filename}.json
+    mv $f ./json/processed/
+  fi
+  i=$[$i+1]
+done
+
+cd ./json/
+python3 filter_json.py
